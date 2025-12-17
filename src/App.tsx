4e549351c3e13f37a2f0aecb9edfcd322ba98c51
dante76 <dante7608@gmail.com>
2025-12-17 17:32:56 +0900

    feat: add initial React component featuring a counter and Vite/React logos

--- src/App.tsx
+++ src/App.tsx
@@ -8,7 +8,7 @@ function App() {
 
   return (
     <>
-      <div className='bg-stone-600'>
+      <div className='bg-amber-200 w-screen flex items-center justify-center '>
         <a href="https://vite.dev" target="_blank">
           <img src={viteLogo} className="logo" alt="Vite logo" />
         </a>
